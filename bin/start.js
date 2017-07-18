64113743dd6d04aee7a30e37ade68917ea44748c
maxgurewitz <maxthegeek1@gmail.com>
2017-07-18 00:55:54 -0700

    koa typescript hello world

---
+++ bin/start.js
@@ -0,0 +1,2 @@
+const app = require('../dist/js/app.js');
+app.listen(3000);
---
+++ src/server/app.ts
@@ -0,0 +1,11 @@
+import * as Koa from 'koa';
+
+const app = new Koa();
+
+const helloWorld : Koa.Middleware = ctx => {
+  ctx.body = 'Hello Koa';
+};
+
+app.use(helloWorld);
+
+module.exports = app;
